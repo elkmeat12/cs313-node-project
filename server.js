7520060dec31ec3e256c7c453066236de7f3ef0e
elkmeat12 <38544362+elkmeat12@users.noreply.github.com>
2020-03-10 16:11:39 -0600

    Adding person query

--- server.js
+++ server.js
@@ -1,19 +1,45 @@
 require('dotenv').config();
+const express = require('express');
+const app = express();
+const { Pool } = require('pg');
 
 const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({connectionString: connectionString});
 
-var sql = "SELECT * FROM person";
+app.set('port', (process.env.PORT || 6789));
+app.use(express.static(__dirname + '/public'));
+app.get('/getPerson', getPerson);
 
-pool.query(sql, function(err, result) {
-   // If an error occured...
-   if (err) {
-      console.log("Error in query: ");
-      console.log(err);
-   }
+app.listen(app.get('port'), function() {
+   console.log('Node app is running on port', app.get('port'));
+});
 
-   // Log to consol for debugging
-   console.log("Back from DB with result:");
-   console.log(result.rows)
-})
+function getPerson(req, res) {
+   const id = req.query.id;
+
+   getPersonFromDB(id, function(error, result) {
+      if (error || result == null || result.length != 1) {
+         res.status(500).json({success: false, data: error});
+      } else {
+         const person = result[0];
+         res.status(200).json(person);
+      }
+   });
+}
+
+function getPersonFromDB(id, callback) {
+   console.log("Getting person from DB with id: " + id);
+   const sql = "SELECT * FROM person WHERE id = $1::int";
+   const stuff = [id];
+
+   pool.query(sql, params, function(err, result) {
+      if (err) {
+         console.log("Error in query: ")
+         console.log(err);
+         callback(err, null);
+      }
+      console.log("Found result: " + JSON.stringify(result.rows));
+      callback(null, result.rows);
+   });
+}
